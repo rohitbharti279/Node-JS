e7551faaa4c2dcd9eb6555996771182bf11edea9
Rohit Bharti <rohitbharti279@gmail.com>
2023-09-22 13:31:52 +0530

    create server

--- index.js
+++ index.js
@@ -3,6 +3,26 @@
 const app = require("./app");
 
 
+//create a local server to receive data from the 
+const http  = require("http");
+
+
+// http.createServer(function).listen(port); //format for creating server
+const port = 8000;
+http.createServer((request, response) => {
+    // response.write(`<h1>hello world!</h1><p> i am at port:- ${port} </p>`);
+    response.write(`hello world! i am at port:- ${port}`);
+    response.end();
+}
+).listen(port);
+
+// const server = http.createServer((req, res) => {
+//     res.write(`<h1>hello india vs australia!</h1> 
+//     <p>now i am at port:- ${port}</p>`);
+//     res.end();
+// })
+// server.listen(port);
+
 console.log(app);
 console.log(app.car1);
 console.log(app.fn1());
@@ -23,3 +43,4 @@ console.log(filtur);
 const fs = require("fs");
 fs.writeFileSync("filename.txt", "write the content in the file here");
 
+console.log("package.json");
