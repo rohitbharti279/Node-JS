bffcf377c62029fa7fa6aaedd523221489b063ae
Rohit Bharti <rohitbharti279@gmail.com>
2023-09-24 13:50:28 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        name: "Rohit Bharti",
+        email: "rohit@gmail.com",
+        age: 23
+    },
+    {
+        name: "Ankit kumar",
+        email: "ankit@gmail.com",
+        age: 10
+    },
+    {
+        name: "rahul kumar",
+        email: "rahul@gmail.com",
+        age: 33
+    },
+    {
+        name: "sam",
+        email: "sam@gmail.com",
+        age: 23
+    }
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,67 +1,30 @@
-// const anyName = require("./app.js");
-// console.log(anyName);
-// const app = require("./app.js");
 
-import color from "colors";
-// const color = require("colors");
+const http = require("http");
+// import http from "http";
 
-// const chalk = require("chalk");
-// import chalk from "./chalk";
-// import Chalk from 'chalk';
+const data = require("./data.js")
+// import {data} from "data";
 
-//create a local server to receive data from the 
-// const http  = require("http");
-import http from "http";
+const port = 5000;
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application\json' });
+    res.write(JSON.stringify(data));
+    res.end();
+}).listen(port);
 
-// http.createServer(function).listen(port); //format for creating server
-const port = 8000;
-http.createServer((request, response) => {
-    // response.write(`<h1>hello world!</h1><p> i am at port:- ${port} </p>`);
-    response.write(`hello world! i am at port:- ${port}, this is my server on port ${port}`);
-    response.end();
+// console.log(process);
+// console.log(process.argv);
+
+const fs = require("fs");
+
+const input = process.argv;
+
+if (input[2] == "add") {
+    fs.writeFileSync(input[3], input[4]);
 }
-).listen(port);
-
-// const server = http.createServer((req, res) => {
-//     res.write(`<h1>hello india vs australia!</h1> 
-//     <p>now i am at port:- ${port}</p>`);
-//     res.end();
-// })
-// server.listen(port);
-
-// console.log(app);
-// console.log(app.car1);
-// console.log(app.fn1());
-
-console.log("Starting");
-const a =5;
-const b= 10;
-console.warn(a+b);
-
-const arr = ["a", "b", "c", "d", "e",2,3,44,44,5,5,5,"5"];
-const filtur = arr.filter((item) => {
-   return item === 5;
-})
-
-console.log(filtur);
-
-// create file
-// const fs = require("fs");
-import fs from "fs";
-fs.writeFileSync("filename.txt", "write the content in the file here");
-
-// colors
-console.log("greencolors".green);
-console.log("rohit bharti".red);
-console.log("rohit bharti".bgBlue); //background color 
-console.log('hello'.green); // outputs green text
-console.log('i like cake and pies'.underline.red) // outputs red underlined text
-console.log('inverse the color'.inverse); // inverses the color
-console.log('OMG Rainbows!'.rainbow); // rainbow
-console.log('Run the trap'.trap); // Drops the bass
-
-//chalk
-// console.log(chalk.blue("hello"));
-
-console.log("hello patna");
-console.log(100+300);
+else if (input[2] == "remove") {
+    fs.unlinkSync(input[3]);
+}
+else {
+    console.log("Error: Unknown");
+}
